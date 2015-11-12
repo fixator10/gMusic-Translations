893be19eb11c9159c4cc2e6fd358c19435b49a86
Connor McFarlane <ConnorMcF@users.noreply.github.com>
2015-11-12 21:14:15 +0000

    Update languages.php

--- languages.php
+++ languages.php
@@ -1,8 +1,8 @@
 <?php
 if($language == "en") { $sh_en == ""; } else { $sh_en == '<li><a onclick="setlang("en");">English</a></li>'; }
-if($language == "cy") { $sh_cy == ""; } else { $sh_cy == "<li><a onclick="setlang("cy");">Cymraeg</a></li>"; }
-if($language == "pt") { $sh_pt == ""; } else { $sh_pt == "<li><a onclick="setlang("pt");">Português Europeu</a></li>"; }
-if($language == "de") { $sh_de == ""; } else { $sh_de == "<li><a onclick="setlang("de");">Deutsche</a></li>"; }
+if($language == "cy") { $sh_cy == ""; } else { $sh_cy == '<li><a onclick="setlang("cy");">Cymraeg</a></li>'; }
+if($language == "pt") { $sh_pt == ""; } else { $sh_pt == '<li><a onclick="setlang("pt");">Português Europeu</a></li>'; }
+if($language == "de") { $sh_de == ""; } else { $sh_de == '<li><a onclick="setlang("de");">Deutsche</a></li>'; }
 $lang["options"] =
 '
 <li><a href="/lang">Help Translate!</a></li>
@@ -10,4 +10,4 @@ $lang["options"] =
 '. $sh_en .'
 '. $sh_cy .'
 '. $sh_pt .'
-'. $sh_de .';
+'. $sh_de;
