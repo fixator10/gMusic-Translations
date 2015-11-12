23f5e5449c378e216f19f14a723a43945da71eee
Connor McFarlane <ConnorMcF@users.noreply.github.com>
2015-11-12 21:12:03 +0000

    Update languages.php

--- languages.php
+++ languages.php
@@ -1,3 +1,4 @@
+<?php
 if($language == "en") { $sh_en == ""; } else { $sh_en == '<li><a onclick="setlang(\"en\");">English</a></li>'; }
 if($language == "cy") { $sh_cy == ""; } else { $sh_cy == "<li><a onclick="setlang(\"cy\");">Cymraeg</a></li>"; }
 if($language == "pt") { $sh_pt == ""; } else { $sh_pt == "<li><a onclick="setlang(\"pt\");">Português Europeu</a></li>"; }
