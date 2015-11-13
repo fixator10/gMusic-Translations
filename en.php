44d92123b49460e8859e958fe53de6bada10a2a7
Connor McFarlane <ConnorMcF@users.noreply.github.com>
2015-11-13 18:49:41 +0000

    Update en.php

--- en.php
+++ en.php
@@ -11,9 +11,9 @@ $lang["options"] = // don't worry about editing this part
 <<<EOT
 <li><a href="/lang">Help Translate!</a></li>
 <li role="separator" class="divider"></li>
-<li><a onclick="setlang('cy');">Cymraeg</a></li>
 <li><a onclick="setlang('pt');">Português Europeu</a></li>
 <li><a onclick="setlang('de');">Deutsche</a></li>
+<li><a onclick="setlang('ru');">Русский</a></li>
 EOT;
 
 // --- Translations
