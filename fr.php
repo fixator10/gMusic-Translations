aeb8422b23fdb69eefe5cfc69eee27f3a1185b11
Gedo789 <gedo789@gmail.com>
2015-11-16 18:17:58 +0100

    Create fr.php

---
+++ fr.php
@@ -0,0 +1,116 @@
+<?php
+error_reporting(E_ALL & ~E_WARNING);
+// Fichier de langage français pour gMusic
+// Par Gedo789
+$lang["author"] = "Gedo789";
+$lang["author_attribution"] = "gedo789.fr.nf";
+$lang["lang"] = "Français";
+$lang["lang_code"] = "fr";
+$lang["options"] =
+<<<EOT
+<li><a onclick="setlang('cy');">Cymraeg</a></li>
+<li><a onclick="setlang('fr');"></a>Français</li>
+<li><a onclick="setlang('pt');">PortuguÃªs Europeu</a></li>
+EOT;
+// --- Traductions
+// Barre latéral
+$lang["name"] = "gMusic";
+$lang["notfound"] = "notFound";
+$lang["title"] = "gMusic";
+$lang["title_caps"] = "gMUSIC";
+$lang["title_subheading"] = "Choissisez votre propre musique!";
+$lang["side_home"] = "Accueil";
+$lang["side_what"] = "Qu'est que c'est?";
+$lang["side_players"] = "Pour les joueurs";
+$lang["side_owners"] = "Pour les propriétaires de serveur";
+$lang["side_devs"] = "Pour les développeurs";
+$lang["side_testimonials"] = "Témoignages";
+$lang["side_songs"] = "Musiques";
+$lang["side_servers"] = "Serveurs";
+$lang["side_dashboard"] = "Tableau de bord";
+$lang["side_logout"] = "Déconnexion";
+// Page d'accueil
+$lang["welcome"] = "Bienvenue!";
+$lang["cdn_by"] = "CDN par ";
+$lang["maxcdn"] = "MaxCDN";
+$lang["contact"] = "Contact";
+$lang["signinwithsteam"] = "Se connecter avec Steam!";
+$lang["what_is_gmusic"] = "QU'EST-CE QUE gMUSIC?";
+$lang["what_is_gmusic_text"] = 
+"Vous détestez les musiques de chargement forcé que les propriétaires de serveur vous force à écouter?
+Avec gMusic, vous pouvez désormais choisir les musiques que vous voulez écoutez!";
+$lang["players"] = "JOUEURS";
+$lang["players_text"] = 
+"Êtes-vous un joueur de Garry's Mod qui désire changer les musiques que vous entendez sur tous les serveurs ou les désativer entièrement?
+Connectez-vous avec Steam en dessous pour aller au panneau de contrôle.";
+$lang["serverowners"] = "PROPRIÉTAIRES DE SERVEUR";
+$lang["serverowners_text"] = 
+"Êtes-vous un propriétaire de serveur qui désire ajouter gMusic à votre écran de chargement?
+Connectez-vous avec Steam en dessous pour aller au panneau de contrôle.";
+$lang["devs"] = "DÉVELOPPEURS";
+$lang["devs_text"] = 
+"Êtes-vous un développeur qui désire intégrer gMusic sur votre site internet?
+Jetez-y un coup d'œil à notre référence de l'API!";
+$lang["devs_button"] = "VISITEZ LA RÉFÉRENCE API";
+$lang["testimonials"] = "TÉMOIGNAGES";
+// Bienvenue!
+$lang["wel_welcome"] = "Bienvenue!";
+$lang["wel_welcometo"] = "Bienvenue à gMusic!";
+$lang["wel_regular"] = "Êtes-vous un joueur régulier de Garry's Mod? <a href='/dashboard/'>Choissisez les musiques que vous voulez écoutez!</a>"; // don't edit the tags, <a> and </a>
+$lang["wel_owner"] = "Êtes-vous un propriétaire de serveur ou développeur? <a href='/dashboard/servers'>Enregistrer votre serveur avec gMusic aujourd'hui!</a>"; // don't edit the tags, <a> and </a>
+// Banni! :C
+$lang["ban_banned"] = "Banni!";
+$lang["ban_uhoh"] = "Oh oh! Il semblerait que vous êtes banni... :C";
+$lang["ban_contact"] = 'N\'hésitez pas à <a href="/contact">me contacter</a> pour faire appel à ce bannissement.';
+// Oh oh! :O
+$lang["uhoh_uhoh"] = "Oh oh!";
+$lang["uhoh_lookslike"] = "Il semblerait que quelque chose s'est mal déroulé!";
+$lang["uhoh_reported"] = "Ne vous en inquiétez pas, l'erreur a été reporté à un développeur, et il le corrigera ou du moins vivement.";
+// Tableau de bord
+$lang["dash_title"] = "TABLEAU DE BORD DE gMUSIC";
+$lang["dash_subheading"] = "Choissisez votre propre musique!";
+$lang["dash_songs"] = "Musiques";
+$lang["dash_servers"] = "Serveurs";
+$lang["dash_addasong"] = "Ajouter une musique!";
+$lang["dash_addnewsong"] = "Ajouter une nouvelle musique";
+$lang["dash_mutesongs"] = "Mettre en mode muet les musiques";
+$lang["dash_unmutesongs"] = "Retirer le mode muet sur les musiques";
+$lang["dash_id"] = "ID";
+$lang["dash_fail"] = "ÉCHEC!";
+$lang["dash_youtube"] = "YouTube";
+$lang["dash_soundcloud"] = "Soundcloud";
+$lang["dash_notimplimented"] = "(non implémenté)";
+$lang["dash_add"] = "Ajouter";
+$lang["dash_duration"] = "Durée";
+$lang["dash_videoid"] = "ID de la vidéo";
+$lang["dash_enable"] = "Activer";
+$lang["dash_disable"] = "Désactiver";
+$lang["dash_delete"] = "Supprimer";
+$lang["dash_cancel"] = "Annuler";
+$lang["dash_email_doconfirm"] = "Veuillez vérifiez votre e-mail pour la confirmation! (Vérifiez vos pourriels si vous ne le retrouvez pas)";
+$lang["dash_email_confirmed"] = 'Merci d\'avoir vérifié votre e-mail! Vous pourriez maintenant <a data-toggle="modal" href="#addServerModal">ajouter un serveur</a>!';
+$lang["dash_email_disposable"] = "Veuillez ne pas utiliser une e-mail jetable, Nous n'enverrons pas de pourriels. Cela ne peut qu'arriver qu'en cas de défauts.";
+$lang["dash_email_notverified"] = "Vous ne pouvez pas ajouter de serveur lorsque votre e-mail n'est pas vérifié!";
+$lang["dash_addaserver"] = "Ajouter un nouveau serveur";
+$lang["dash_addserver"] = "Ajouter un serveur!";
+$lang["dash_name"] = "Nom";
+$lang["dash_loadingurl"] = "URL de l'écran de chargement";
+$lang["dash_optional"] = "Optionnel";
+$lang["dash_email_verifytitle"] = "Vérifiez votre e-mail!";
+$lang["dash_email"] = "E-mail";
+$lang["dash_email_wontsell"] = "Nous ne la vendrons pas, jamais!";
+$lang["dash_email_sendconfirmation"] = "Envoyer la confirmation";
+$lang["dash_songs_deletemsg"] = // dessous
+'<p>Êtes-vous sûre de vouloir supprimer <b><?php echo($songtitle); ?></b>?</p>
+<p>Vous ne l\'entendrez plus durant la connexion si vous le faites</p>'; // don't edit the tags, <b> to </b>
+$lang["dash_selectoradd"] = // dessous
+'Veuillez <a data-toggle="modal" href="#addSongModal">ajouter une musique</a> ou en sélectionner une existante depuis le côté gauche.<br/>
+Une musique aléatoire sera sélectionner pour être lu lorsque vous rejoindrez un serveur.'; // don't edit the tags, <a> to </a>
+$lang["dash_serverselectoradd"] = // dessous
+'Veuillez <a data-toggle="modal" href="#<?php echo($modal); ?>">ajouter un serveur</a> ou en sélectionner une existante depuis le côté gauche.'; // don't edit the tags, <a> to </a>
+// 404
+$lang["404"] = "404";
+$lang["404_uhoh"] = "Oh oh! Ceci n'était pas ce que voulez voir...";
+$lang["404_contact"] = 'Si vous venez d\'ici depuis un lien valide, veuillez <a href="/contact">me le faire savoir</a> ou alors, vous pouvez <a href="/">retourner à la page d\'accueil</a>.'; // don't edit the tags, <a> and </a> (edit inside)
+// Etc
+$lang["contact"] = "Contact";
