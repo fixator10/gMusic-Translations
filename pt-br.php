519d90c575e6e111966ea86d520621be7b1342c9
GGG KILLER <gggkiller2@gmail.com>
2015-12-31 08:15:05 -0200

    Create pt-br.php

---
+++ pt-br.php
@@ -0,0 +1,174 @@
+// <?php
+// Keep the starting tag commented else eval will fail! :O
+// -------------------------------------------------------
+// Brazilian Portuguese language file for gMusic
+// By GGG KILLER
+$lang["author"] = "GGG KILLER";
+$lang["author_attribution"] = "gggkiller.com";
+$lang["lang"] = "Português Brasileiro";
+$lang["lang_code"] = "pt-br";
+$lang["options"] = // don't worry about editing this part
+<<<EOT
+<li><a href="/lang">Ajude a Traduzir!</a></li>
+<li role="separator" class="divider"></li>
+<li><a onclick="setlang('pt-br');">Português Brasileiro</a></li>
+<li><a onclick="setlang('pt');">Português Europeu</a></li>
+<li><a onclick="setlang('de');">Deutsche</a></li>
+<li><a onclick="setlang('ru');">Русский</a></li>
+<li><a onclick="setlang('fr');">Français</a></li>
+EOT;
+// --- Translations
+// Sidebar
+$lang["name"] = "gMusic";
+$lang["notfound"] = "notFound";
+$lang["title"] = "gMusic";
+$lang["title_caps"] = "gMUSIC";
+$lang["title_subheading"] = "Escolha a sua música!";
+$lang["side_home"] = "Início";
+$lang["side_what"] = "O quê é?";
+$lang["side_players"] = "Para Jogadores";
+$lang["side_owners"] = "Para Donos";
+$lang["side_devs"] = "Para Desenvolvedores";
+$lang["side_testimonials"] = "Testemunhos";
+$lang["side_premium"] = "Premium";
+$lang["side_stats"] = "Estatisticas";
+$lang["side_songs"] = "Músicas";
+$lang["side_servers"] = "Servidores";
+$lang["side_dashboard"] = "Painel de Controle";
+$lang["side_logout"] = "Sair";
+// Homepage
+$lang["welcome"] = "Bem Vindo!";
+$lang["cdn_by"] = "CDN por ";
+$lang["maxcdn"] = "MaxCDN";
+$lang["contact"] = "Contato";
+$lang["signinwithsteam"] = "Faça login com Steam!";
+$lang["stats"] = "ESTATISTICAS";
+$lang["stats_songs"] = "Total de músicas registradas com gMusic";
+$lang["stats_users"] = "Total de usuários registrados com gMusic";
+$lang["stats_servers"] = "Total de servidores registrados com gMusic";
+$lang["stats_betas"] = "Total de servidores registrados com gMusic beta";
+$lang["what_is_gmusic"] = "O QUÊ É gMUSIC?";
+$lang["what_is_gmusic_text"] = 
+"Odeia ter que ouvir a música de carregamento forçada me você pelos donos de servidores?
+Agora com gMusic, você pode escolher o quê você quer ouvir!";
+$lang["players"] = "JOGADORES";
+$lang["players_text"] = 
+"Você é um jogador de Garry's Mod procurando por mudar a música que você ouve em todos os servidores ou desabilitá-la por inteiro?
+Faça login com o Steam abaixo para continuar para o painel de controle.";
+$lang["serverowners"] = "DONOS DE SERVIDORES";
+$lang["serverowners_text"] = 
+"Você é um dono de servidor procurando adicionar gMusic à tela de carregamento do seu servidor?
+Faça login com o Steam abaixo para continuar para o painel de controle.";
+$lang["devs"] = "DESENVOLVEDORES DE SERVIDORES";
+$lang["devs_text"] = 
+"Você é um desenvolvedor de um servidor procurando integrar gMusic com o seu website?
+Dê uma olhada na documentação da nossa API!";
+$lang["devs_button"] = "VISITAR DOCUMENTAÇÃO DA API";
+$lang["testimonials"] = "TESTEMUNHOS";
+// Welcome!
+$lang["wel_welcome"] = "Bem Vindo!";
+$lang["wel_welcometo"] = "Bem Vindo a gMusic!";
+$lang["wel_regular"] = "Você joga Garry's Mod regularmente? <a href='/dashboard/'>Escolha as músicas que você quer ouvir!</a>"; // don't edit the tags, <a> and </a>
+$lang["wel_owner"] = "Você é um dono de servidor ou desenvolvedor? <a href='/dashboard/servers'>Registre seu servidor com gMusic hoje!</a>"; // don't edit the tags, <a> and </a>
+// Banned! :C
+$lang["ban_banned"] = "Banido!";
+$lang["ban_uhoh"] = "Uh oh! Parece que você foi banido... :C";
+$lang["ban_contact"] = 'Sinta-se a vontade para <a href="/contact">me contatar</a> para justificar-se.';
+// Uh oh! :O
+$lang["uhoh_uhoh"] = "Uh oh!";
+$lang["uhoh_lookslike"] = "Parece que algo deu errado!";
+$lang["uhoh_reported"] = "Não se preoucupe, o erro foi reportado a um desenvolvedor e ele irá consertá-lo, provavelmente.";
+// Dashboard
+$lang["dash_title"] = "PAINEL DE CONTROLE gMUSIC";
+$lang["dash_subheading"] = "Escolha sua própia música!";
+$lang["dash_songs"] = "Músicas";
+$lang["dash_servers"] = "Servidores";
+$lang["dash_addasong"] = "Adicione uma música!";
+$lang["dash_addnewsong"] = "Adicionar nova música";
+$lang["dash_mutesongs"] = "Desativar músicas";
+$lang["dash_unmutesongs"] = "Ativar músicas";
+$lang["dash_cancel"] = "Cancelar";
+$lang["dash_id"] = "ID";
+$lang["dash_fail"] = "FALHA!";
+$lang["dash_youtube"] = "YouTube";
+$lang["dash_soundcloud"] = "Soundcloud";
+$lang["dash_notimplimented"] = "(não implementado)";
+$lang["dash_add"] = "Adicionar";
+$lang["dash_duration"] = "Duração";
+$lang["dash_videoid"] = "ID do Vídeo";
+$lang["dash_enable"] = "Habilitar";
+$lang["dash_disable"] = "Desabilitar";
+$lang["dash_delete"] = "Deletar";
+$lang["dash_cancel"] = "Cancelar";
+$lang["dash_reset"] = "Resetar";
+$lang["dash_email_doconfirm"] = "Por favor, cheque seu email pela mensagem de confirmação! (Cheque sua lixeira se você não conseguir achá-la)";
+$lang["dash_email_confirmed"] = 'Obrigado por verificar seu email!! Você agora pode <a data-toggle="modal" href="#addServerModal">adicionar um servidor</a>!';
+$lang["dash_email_disposable"] = "Por favor, não use um email descartável, nos nunca iremos lhe mandar spam. Somente caso algo dê errado.";
+$lang["dash_email_notverified"] = "Você não pode adicionar um servidor, já que seu email não foi verificado ainda!";
+$lang["dash_addaserver"] = "Adicionar novo servidor";
+$lang["dash_addserver"] = "Adicione um servidor!";
+$lang["dash_name"] = "Nome";
+$lang["dash_loadingurl"] = "URL da Página de Carregamento";
+$lang["dash_optional"] = "Opcional";
+$lang["dash_email_verifytitle"] = "Verificar seu email!";
+$lang["dash_email"] = "Email";
+$lang["dash_email_wontsell"] = "Nós não o venderemos nunca!!";
+$lang["dash_email_sendconfirmation"] = "Enviar Confirmação";
+$lang["dash_actions"] = "Acões";
+$lang["dash_songs_deletemsg"] = // below
+'<p>Você tem certeza que deseja deletar <b><?php echo($songtitle); ?></b>?</p>
+<p>Você não vai mais poder ouvir esta música enquando conecta se o fizer!</p>'; // don't edit the tags, <b> to </b>
+$lang["dash_selectoradd"] = // below
+'Por favor <a data-toggle="modal" href="#addSongModal">adicione uma música</a> ou selecione uma existente do lado esquerdo.<br/>
+Uma música aleatória será tocada quando você entrar em um servidor.'; // don't edit the tags, <a> to </a>
+$lang["dash_serverselectoradd"] = // below
+'Por favor <a data-toggle="modal" href="#<?php echo($modal); ?>">adicione um servidor</a> ou selecione um existe do lado esquerdo.'; // don't edit the tags, <a> to </a>
+// Servers
+$lang["dash_resetapi"] = "RESETAR CHAVE DE API";
+$lang["dash_setupinstructions"] = "Instruções de configuração";
+$lang["dash_setupinstructions_text"] = // don't worry about editing this part
+<<<EOT
+<ul>
+  <li>Você vai precisar que o host onde você hospeda sua página de carregamento rode PHP para gMusic funcionar corretamente.</li>
+  <li>Se você não tem PHP, você pode utilizar uma linguagem de programação diferente, tal como ASP, desde que o SteamID esteja sendo preenchido corretamente!</li>
+</ul>
+<ol>
+  <li>Ache o arquivo para a sua página de carregamento.</li>
+  <li>Se você já possui uma música na sua página de carregamento, troque a tag &lt;audio&gt; ou o quê você estiver usando para tocar músicas com o código encontrado na aba de Códigos Embutidos. </li>
+  <li>Se a sua página de carregamento ainda não tem música, coloque o código embutido no final próximo a tag &lt;/body&gt; tag.</li>
+</ol>
+EOT;
+$lang["dash_embedcode"] = "Códigos Embutidos";
+$lang["dash_customjscode"] = "Código JS Customizado";
+$lang["dash_customjscode_text"] = "O código em javascript abaixo está sendo deprecado! Por favor use a, em breve disponível, <a href='/api'>API</a> ao invés!";
+$lang["dash_serverdelete"] = // below 
+"
+<p>Você tem certeza que deseja deletar <b>%s</b>?</p>
+<p>Qualquer música na sua página de carregamento irá parar de funcionar!</p>
+";
+$lang["dash_resetapikey_modal_title"] = "Resetar chave da API?";
+$lang["dash_resetapikey_modal_text"] = // don't worry about editing this part
+<<<EOT
+<p>Você tem certeza que deseja resetar a chave da API para <b>%s</b>?</p>
+<p>Lembre de atualizar a chave de API nos seus códigos!</p>
+EOT;
+$lang["dash_totalconnects"] = "Conexões totais";
+// Servers - embed widget
+$lang["dash_embedwidget"] = "Incluir Widget";
+$lang["dash_embedwidget_text"] = "Oi! Eu estou literalmente fazendo o widget neste exato segunto! Você provavelmente não deveria tocar isso porquê algo pode quebrar provavelmente.";
+// Premium
+$lang["premium"] = "Premium";
+$lang["premium_text"] = // below 
+"
+<p>Você quer ajudar a suportar o desenvolvimento de gMusic?</p>
+<p>Quando você compra o premium, a marca é removida do widget, a habilidade de ver visitas únicas da sua página de carregamento e eu irei re-fazer o design do widget para combinar com seu site e mais!</p>
+<p>Pagamento único de £3.50</p>
+";
+$lang["premium_already"] = "Você já é premium!";
+$lang["premium_notloggedin"] = "Você não está logado!";
+// 404
+$lang["404"] = "404";
+$lang["404_uhoh"] = "Uh oh! Isso não é o quê você estava procurando...";
+$lang["404_contact"] = 'Se você veio aqui de um link válido, por favor <a href="/contact">conte-me</a> ou você pode <a href="/">retornar à página inicial</a>.'; // don't edit the tags, <a> and </a> (edit inside)
+// Etc
+$lang["contact"] = "Contato";
